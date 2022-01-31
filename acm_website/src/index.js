f6ed4c3bb712ef1bdcc4900c5c1e3c2996ab8e76
sarthakakre27 <sarthakakre27@gmail.com>
2022-01-31 23:45:29 +0530

    commented other features, current focus on Home Page, gitignore added

--- acm_website/src/index.js
+++ acm_website/src/index.js
@@ -3,6 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import "./codepen.css";
+// import "./codepen.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
