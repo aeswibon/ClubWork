5f17ec5e15d878e3a2d3783697c9cc3988fc9cb2
Sushant Kogurwar <Sushantkogurwar@gmail.com>
2022-02-05 15:02:49 +0530

    updated logo and added shadows

--- acm_website/src/Components/UI/preloader/preloader.js
+++ acm_website/src/Components/UI/preloader/preloader.js
@@ -12,16 +12,16 @@ function Preloader() {
     <div id={preloader.preloader}>
       <div className="align-items-center justify-content-center text-center ">
          <img
-          src="https://i.postimg.cc/bvDd48tV/logo-1.png"
-          className="m-5"
-          data-aos="zoom-out-up" data-aos-delay="300"
-          style={{ height: "13vh" }}
+          src="./image/acmlogo.png"
+          className="m-5 "
+          data-aos="zoom-out" data-aos-delay="300" data-aos-duration="2000"
+          style={{ height: "20vh",filter:" drop-shadow(5px 5px 5px #ffffff)" }}
           alt="logo-1"
         />
 
-        <h1 className="text-bold text-white home_text" data-aos="fade-right" data-aos-delay="300" data-aos-easing="ease-in-sine" >
+        <h1 className="text-bold text-white home_text  " style={{ textShadow: "1px 1px 3px #000000"}} data-aos="fade-right" data-aos-delay="300" data-aos-easing="ease-in-sine" >
         welcome to  
-        </h1><h1 data-aos="fade-right" data-aos-delay="300" data-aos-easing="ease-in-sine"  className="text-bold  home_text" style={{color : "blue"}}>ACM</h1>
+        </h1><h1 data-aos="fade-right" data-aos-delay="300" data-aos-easing="ease-in-sine"  id={preloader.acm} className="text-bold  home_text  " style={{color : "#276cce", textShadow: "1px 1px 3px #000000"}}>ACM</h1>
       </div>
       
  
